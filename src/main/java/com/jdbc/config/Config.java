25c8603f84e7d13d977f02b2639681af866679b4
Rakesh madu <rakeshmadu24@gmail.com>
2022-09-19 22:04:26 +0530

    Update Config.java

--- src/main/java/com/jdbc/config/Config.java
+++ src/main/java/com/jdbc/config/Config.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class Config {
 
-	static final String URL = "jdbc:mysql://localhost:3306/payroll_service";
+	static final String URL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
 	static final String USER_NAME = "root";
 	static final String PASSWORD = "";
 	
